4aa095a288868beefd5b16b42c7db267396f99f3
sahithyabathini5 <sahithyabathini5@dal.ca>
2019-05-23 11:48:23 -0300

    create ticket

--- Task4.py
+++ Task4.py
@@ -1,10 +1,10 @@
 controller='devnetapi.cisco.com/sandbox/apic.em'
 create_ticket= __input__("create_ticket")
-def getconnecthosts(ticket):
+def getcounthosts(ticket):
     t=ticket['response']['serviceticket']
     print(t)
     url = "https://" + controller + "/api/v1/host?limit=1&offset=1"
     header = {"content-type": "application/json", "X-Auth-Token":ticket}
     response = requests.get(url, headers=header, verify=False)
-
-getconnecthosts(tickets)
+    print("network count is ", r,json()['response'])
+getcountthosts(tickets)
