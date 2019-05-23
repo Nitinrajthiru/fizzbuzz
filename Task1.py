e752317934f810527fe605ad12eca598f93d747e
sahithyabathini5 <sahithyabathini5@dal.ca>
2019-05-23 10:27:23 -0300

    couting th alphabets

---
+++ Task1.py
@@ -0,0 +1,33 @@
+fin = open("Book1.txt")
+fout = open("copy1.txt", "w+")
+count = 0
+page = 0 
+pageno = int(input("enter a page number"))
+for i in range(page)
+    count=0:
+    for i,val in enumerate(fin):
+        s = val.strip("/n")
+        for j in s:
+            print(j)
+            if j== 'O' or j== 'o':
+                     h= '0'
+                     fout.write(h)
+            elif j== 'E' or j== 'e'
+                     h= '3'
+                     fout.write(h)
+            elif j== 'I' or j== 'i'
+                     h= '1'
+                     fout.write(h)
+           else:
+                     fout.write(j)
+
+
+          fout.write('/n')
+          print(s)
+          count +=1
+         if count==25:
+             break
+   page +=1
+
+fout.close()
+
